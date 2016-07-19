755c9445d9cd59fc723c40d501bcd7ee3b96eb37
Cam Wiegert <cam@camwiegert.com>
2016-07-19 12:22:15 -0500

    Add banner to webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+const webpack = require('webpack'),
+      package = require('./package');
+
+const banner = `Scrambler.js ${package.version} - ${package.description}\nCopyright (c) ${ new Date().getFullYear() } ${package.author} - ${package.homepage}\nLicense: ${package.license}`;
+
 module.exports = {
     context: __dirname + '/src',
     entry: './index.js',
@@ -13,5 +18,8 @@ module.exports = {
             exclude: /node_modules/,
             loader: 'babel-loader'
         }]
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin(banner)
+    ]
 };
