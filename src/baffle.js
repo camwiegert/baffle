1777b232536031ba105aa8f942d9e1da27444722
Cam Wiegert <cam@camwiegert.com>
2016-07-28 10:10:11 -0500

    Add refactored interface

--- src/baffle.js
+++ src/baffle.js
@@ -1,2 +1,50 @@
 import Obfuscator from './obfuscator';
-export default Obfuscator;
+
+import {
+    each,
+    extend,
+    getElements
+} from './utils';
+
+const defaults = {
+    characters: 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz~!@#$%^&*()-+=[]{}|;:,./<>?'.split(''),
+    speed: 50
+};
+
+class Baffle {
+
+    constructor(elements, options) {
+        this.options  = extend(Object.create(defaults), options);
+        this.elements = getElements(elements).map(Obfuscator);
+        this.running  = false;
+    }
+
+    once(chars = this.options.characters) {
+        each(this.elements, el => el.write(chars));
+        return this;
+    }
+
+    start() {
+        clearInterval(this.interval);
+        this.interval = setInterval(() => this.once(), this.options.speed);
+        this.running = true;
+        return this;
+    }
+
+    stop() {
+        clearInterval(this.interval);
+        each(this.elements, el => el.write());
+        this.running = false;
+        return this;
+    }
+
+    set(opts) {
+        extend(this.options, opts);
+        if (this.running) this.start();
+        return this;
+    }
+
+}
+
+// Export a factory function so we don't need 'new'.
+export default (elements, options) => new Baffle(elements, options);
